df1c1bedbcb3b16c8bbb515ad518b1a5267626e0
Joshua Hiscocks <62932202+jhisc@users.noreply.github.com>
2021-04-12 19:38:02 +0100

    Added Lease details to output

--- output.tf
+++ output.tf
@@ -26,3 +26,12 @@ output "client_secret" {
   value       =  data.vault_azure_access_credentials.creds.client_secret
 }
 
+output "lease_duration" {
+  description = "Azure lease duration"
+  value       =  data.vault_azure_access_credentials.creds.lease_duration
+}
+
+output "lease_start_time" {
+  description = "Azure lease start time"
+  value       =  data.vault_azure_access_credentials.creds.lease_start_time
+}
